4e64f1db461c8400c1b434d13bcab02f4b928041
RD\wufan <adfihvg.fijd@gmail.com>
2018-02-02 13:03:22 +0800

    整合ssm框架完成初始化

--- src/main/java/com/mmall/service/IUserService.java
+++ src/main/java/com/mmall/service/IUserService.java
@@ -1,7 +1,6 @@
 package com.mmall.service;
 
 import com.mmall.common.ServerResponse;
-import com.mmall.pojo.User;
 
 /**
  * Created by geely
--- src/main/java/com/mmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -3,13 +3,10 @@ package com.mmall.service.impl;
 import com.mmall.common.Const;
 import com.mmall.common.ServerResponse;
 import com.mmall.common.TokenCache;
-import com.mmall.dao.UserMapper;
-import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
 import com.mmall.util.MD5Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 import java.util.UUID;
 
